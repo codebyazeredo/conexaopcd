2b7d021f9c5a76973ed3f34bf6cd52ff593acfae
Matheus Azeredo <matheus_a_santos@estudante.sc.senai.br>
2024-08-02 22:07:40 -0300

    Adição das funcionalidades do campo de escolha de sexo e alterações no escopo geral

--- src/main/java/com/example/demo/Form/Pessoa/PessoaForm.java
+++ src/main/java/com/example/demo/Form/Pessoa/PessoaForm.java
@@ -1,25 +1,40 @@
 package com.example.demo.Form.Pessoa;
-
+import com.example.demo.Enum.Sexo;
 import com.example.demo.Model.Pessoa;
-
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
 public class PessoaForm {
 
-    @NotBlank(message = "Preencha o campo nome.")
+    @NotBlank(message = "Preencha os campos.")
     private String nome;
 
+    @NotNull(message = "Preencha a data de Nascimento.")
+    @Past(message = "Data de nascimento deve ser no passado.")
+    @JsonFormat(pattern = "dd-mm-yyyy")
+    private LocalDate nascimento;
 
-    public Pessoa toEntity(){
-        return new Pessoa(nome);
+    @NotNull(message = "Preencha o campo Sexo.")
+    private int sexo;
+
+
+    public Pessoa toEntity() {
+
+        Sexo sexo = Sexo.fromCodigo(this.sexo);
+        return new Pessoa(nome, nascimento, sexo);
     }
 
-    public PessoaForm(Pessoa pessoa){
+    public PessoaForm(Pessoa pessoa) {
         this.nome = pessoa.getNome();
+        this.nascimento = pessoa.getNascimento();
     }
-
 }
+
